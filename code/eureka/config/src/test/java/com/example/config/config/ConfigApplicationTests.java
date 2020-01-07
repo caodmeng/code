6277650df87180c24dd88001b41cd752eb388e0c
caodmeng <caodmeng@163.com>
2020-01-07 16:02:57 +0800

    配置中心

---
+++ code/eureka/config/src/test/java/com/example/config/config/ConfigApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.config.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ConfigApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
