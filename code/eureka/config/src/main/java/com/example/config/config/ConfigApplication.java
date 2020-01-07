521959e5fc07de12d54188be3bb411b853bd6126
caodmeng <caodmeng@163.com>
2020-01-07 16:12:46 +0800

    样式修改

--- code/eureka/config/src/main/java/com/example/config/config/ConfigApplication.java
+++ code/eureka/config/src/main/java/com/example/config/config/ConfigApplication.java
@@ -3,7 +3,8 @@ package com.example.config.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication public class ConfigApplication {
+@SpringBootApplication
+public class ConfigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
