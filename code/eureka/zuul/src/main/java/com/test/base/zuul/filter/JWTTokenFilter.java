8bae541b9193fa493d8c439680c1248176706090
caodmeng <caodmeng@163.com>
2020-01-08 18:01:49 +0800

    配置中心

--- code/eureka/zuul/src/main/java/com/test/base/zuul/filter/JWTTokenFilter.java
+++ code/eureka/zuul/src/main/java/com/test/base/zuul/filter/JWTTokenFilter.java
@@ -1,4 +1,36 @@
 package com.test.base.zuul.filter;
 
-public class JWTTokenFilter {
+import com.alibaba.fastjson.serializer.FilterUtils;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户认证校验过滤器
+ */
+@Component
+public class JWTTokenFilter extends ZuulFilter {
+
+    private static final int FILTER_ORDER = 1;
+  /*  @Autowired
+    private FilterUtils filterUtils;
+    @Autowired
+    private ServiceConfig serviceConfig;*/
+
+    @Override public String filterType() {
+        return null;
+    }
+
+    @Override public int filterOrder() {
+        return 0;
+    }
+
+    @Override public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override public Object run() throws ZuulException {
+        return null;
+    }
 }
