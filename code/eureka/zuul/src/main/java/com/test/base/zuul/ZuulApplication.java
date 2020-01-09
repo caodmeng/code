4b1aef7d6785249756def1124113ff113cdcbd57
caodmeng <caodmeng@163.com>
2020-01-09 10:49:59 +0800

    配置中心配置文件

--- code/eureka/zuul/src/main/java/com/test/base/zuul/ZuulApplication.java
+++ code/eureka/zuul/src/main/java/com/test/base/zuul/ZuulApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 @Configuration
+/*@EnableZuulServer*/
 public class ZuulApplication {
 
 	public static void main(String[] args) {
