e6be551f3256dcd0e233ff5d8470b96d8440b33c
caodmeng <caodmeng@163.com>
2020-01-17 16:55:44 +0800

    新增日志包及链路追踪配置

---
+++ code/eureka/cons/src/main/java/com/test/base/cons/service/impl/CreateSpan.java
@@ -0,0 +1,16 @@
+package com.test.base.cons.service.impl;
+
+import brave.Span;
+import brave.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
+
+public class CreateSpan {
+   /* @Autowired
+    private Tracer tracer;
+    @NewSpan
+    protected void createMySpan(){
+        //创建一个新的span,如果当前线程中已存在span,那么作为mySpan的父span
+       *//* Span mySpan = this.tracer.*//*
+    }*/
+}
